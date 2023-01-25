293765f81175f016a344d2089463d2207b37f458
NazarDaineko <115698927+NazarDaineko@users.noreply.github.com>
2023-01-25 12:01:21 +0500

    variables renamed

--- Work3_5/Program.cs
+++ Work3_5/Program.cs
@@ -1,34 +1,38 @@
-﻿int P3 = 0;
-int P4 = 0;
-int P5 = 0;
+﻿using System;
+
+int enter = 0;
+int rangeNumber = 0;
+int target = 0;
+int userTry = 0;
 Random Rand = new Random();
 
 //Угадайка с пользовательским диапозоном.
 
+Console.WriteLine("Добро пожаловать в Угадайку. В этой версии число будет сгенерировано в диапозоне от 0 до числа, которое зададите вы.");
+
 while (true)
 {
-    Console.WriteLine("Добро пожаловать в Угадайку. В этой версии число будет сгенерировано в диапозоне от 0 до числа, которое зададите вы.");
     Console.WriteLine("Для старта введите 0. Для выхода введите -1.");
-    P3 = int.Parse(Console.ReadLine());
-    if (P3 == -1) break;
-    else
+    enter = int.Parse(Console.ReadLine());
+    if (enter == -1) break;
+    else if (enter == 0) 
     {
 
         Console.WriteLine("Введите число для генерации загадки.");
-        P3 = int.Parse(Console.ReadLine());
-        P4 = Rand.Next(0, P3);
-        Console.WriteLine("Число сгенерировано. Если вы устанете, введите -1.");
+        rangeNumber = int.Parse(Console.ReadLine());
+        target = Rand.Next(0, rangeNumber);
+        Console.WriteLine("Число сгенерировано. Если вы устанете, оставьте строку пустой.");
         while (true)
         {
             Console.WriteLine("Введите число.");
-            P5 = int.Parse(Console.ReadLine());
-            if (P5 == -1) { Console.WriteLine("Вы устали? Ничего страшного. В следующий раз повезёт. Число было " + P4); break; }
-            if (P5 < P4) Console.WriteLine("Число меньше загаданного.");
-            else if (P5 > P4) Console.WriteLine("Число больше загадонного.");
-            else { Console.WriteLine("Поздравляем, Вы угадали!"); break; }
+            var userInput = Console.ReadLine();
+            if (userInput == "") { Console.WriteLine("Вы устали? Ничего страшного. В следующий раз повезёт. Число было " + target); break; }
+            userTry = int.Parse(userInput);
+            if (userTry < target) Console.WriteLine("Число меньше загаданного.");
+            else if (userTry > target) Console.WriteLine("Число больше загадонного.");
+            else { Console.WriteLine("Поздравляем, Вы угадали!"); Console.ReadLine(); break; }
         }
     }
-    Console.ReadLine();
 }
 Console.WriteLine("Приложение остановлено.");
 Console.ReadLine();
